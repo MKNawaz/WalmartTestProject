91ba12f01b7c542f62d5529d23566d2519ff6c36
Muhammad Khurram Nawaz <khurramn@live.com>
2025-06-18 12:57:33 -0400

    UI Update

---
+++ WalmartTestProject/AppScreens/CountryListViewController/TableViewCell/CountryListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CountryListTableViewCell.swift
+//  WalmartTestProject
+//
+//  Created by Khurram Nawaz on 6/18/25.
+//
+
+import UIKit
+
+class CountryListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
