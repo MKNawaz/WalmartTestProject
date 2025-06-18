ccc983e8cbd19a5663856bf3f9d148b62ff97801
Muhammad Khurram Nawaz <khurramn@live.com>
2025-06-17 21:53:51 -0400

    Initial Commit

---
+++ WalmartTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalmartTestProject
+//
+//  Created by Khurram Nawaz on 6/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WalmartTestProjectTests/WalmartTestProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  WalmartTestProjectTests.swift
+//  WalmartTestProjectTests
+//
+//  Created by Khurram Nawaz on 6/17/25.
+//
+
+import Testing
+@testable import WalmartTestProject
+
+struct WalmartTestProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
